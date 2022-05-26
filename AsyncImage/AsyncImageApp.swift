f7b83033a9f9cbc1618ef890a5ea31f56abc41f5
PavanSisode <pavansisode@gmail.com>
2022-05-26 13:27:47 +0530

    Initial commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Pavan Shisode on 22/04/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
